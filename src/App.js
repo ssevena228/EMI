b588f8320049de53b5b8bfdc2f5c68897ce6acbb
Aman Shukla <ssevena228@gmail.com>
2026-01-21 18:57:33 +0530

    Add EMI calculator with live input and proper calculation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [principle, setPrinciple] = useState(0);
+
+  const [interest, setinterest] = useState(0);
+
+  const [year, setYear] = useState(0);
+
+  const [emi, setEMI] = useState(0);
+
+
+
+
+
+  const handleChande = (e) => {
+
+    console.log(e.target.id, e.target.value);
+
+    const id = e.target.id
+    const value = Number(e.target.value);
+    if (id === 'principle') {
+      setPrinciple(value);
+    }
+    else if (id === 'interest') {
+      setinterest(value);
+    } else {
+      setYear(value);
+    }
+
+  }
+  console.log(principle, interest, year);
+
+
+  //  P(r(1+r)^n/((1+r)^n)-1))
+  const calculateEMI = () => {
+    let r = interest;
+
+
+    if (principle && r && year) {
+      r = r / 12 / 100  //per month
+      const calPow = Math.pow(1 + r, year * 12);
+      const amount = principle * ((r * calPow) / (calPow - 1));
+      setEMI(amount);
+      console.log(emi)
+
+    }
+
+  }
+
+  useEffect(() => {
+
+    calculateEMI();
+
+  }, [principle, interest, year]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='loan-calc'>
+      <h1>Mortgage Calulator</h1>
+
+
+      <div className='inputes'>
+        <p>Principle :</p>
+        <input
+          onChange={handleChande}
+          type='number'
+          id='principle'
+          value={principle}
+        />
+
+        <p>Interest:</p>
+        <input
+          onChange={handleChande}
+          type='number'
+          id='interest'
+          value={interest}
+        />
+
+
+        <p>Years:</p>
+        <input
+          onChange={handleChande}
+          type='number'
+          id='year'
+          value={year}
+        />
+
+
+      </div>
+
+
+
+      <div className='output'>
+
+        Your EMI is {emi}
+
+      </div>
+
     </div>
   );
 }
